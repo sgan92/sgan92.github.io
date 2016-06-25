d07190b4c26e70f50e3e6a603e87ff74d3f3bc87
Stephanie <stephanie.cai.gan@gmail.com>
2016-06-24 21:04:35 -0400

    fix img

--- bundle.js
+++ bundle.js
@@ -87,23 +87,23 @@
 	
 	  this.canvas = document.getElementById("background");
 	  this.context = this.canvas.getContext("2d");
-	  this.gameOver = false;
+	  this.gameOver = true;
 	
 	  this.startScreen();
 	  this.generateClouds();
-	
-	  this.fn = this.startGame.bind(this);
-	  document.addEventListener("keyup", this.fn, false);
 	};
 	
 	Game.prototype = {
 	  startScreen: function () {
+	    this.fn = this.startGame.bind(this);
+	    document.addEventListener("keyup", this.fn, false);
 	    this.menu = new Menu(false, 0);
 	    window.setInterval(this.generateClouds.bind(this), 5000);
 	  },
 	
 	  startGame: function (e) {
-	    if (e.keyCode === 13) {
+	    if (e.keyCode === 13 && this.gameOver) {
+	      document.removeEventListener("keyup", this.fn, false);
 	      this.gameOver = false;
 	      this.context.clearRect(0, 0, 800, 1000);
 	
@@ -122,7 +122,6 @@
 	
 	      this.addBall = window.setInterval(this.addPokeBalls.bind(this), 4567);
 	      this.addBuilding = window.setInterval(this.addMoreObstacles.bind(this), 6000);
-	      document.removeEventListener("keyup", this.fn);
 	    }
 	  },
 	
@@ -135,10 +134,10 @@
 	
 	  checkCollision: function (array, type) {
 	    array.forEach(function (item) {
-	      if (item.position.x <= -500) {
+	      if (item.position.x <= 500) {
 	        array.splice(array.indexOf(item), 1);
 	      } else {
-	        if (this.isBetween(type, item.position.x, item.position.y, item) || this.pokemon.position.y < -100 || this.pokemon.position.y > 1200) {
+	        if (this.isBetween(type, item.position.x, item.position.y, item) || this.pokemon.position.y < 100 || this.pokemon.position.y > 1200) {
 	          this.gameOver = true;
 	        }
 	      }
@@ -148,25 +147,49 @@
 	  isBetween: function (type, itemX, itemY, item) {
 	    let width;
 	    let height;
+	    let hitPokemon = false;
 	
 	    if (type === "pokeball") {
-	      width = 22;
-	      height = 22;
+	      hitPokemon = this.checkPokeBalls(itemX, itemY);
 	    } else if (type === "building") {
-	      width = 395;
-	      height = 270;
+	      hitPokemon = this.checkBuildings(itemX, itemY);
 	    } else if (type === "balloon") {
-	      width = 340;
-	      height = 180;
+	      hitPokemon = this.checkBalloons(itemX, itemY);
 	    }
 	
-	    if (!(this.pokemon.position.x > itemX + width || this.pokemon.position.x + this.pokemon.width < itemX || this.pokemon.position.y > itemY + height || this.pokemon.position.y + this.pokemon.height < itemY)) {
+	    if (hitPokemon) {
 	      return true;
-	    } else if (type === "building" && this.pokemon.position.x >= itemX + width) {
+	    } else if (type === "building" && this.pokemon.position.x >= itemX + 400) {
 	      this.buildings.splice(this.buildings.indexOf(item), 1);
 	      this.menu.currentScore += 1;
 	      this.menu.startScoring();
 	      return false;
+	    }
+	  },
+	
+	  checkBalloons: function (xPos, yPos) {
+	    if (this.checkHitBoxes(xPos, yPos172, 323, 233) || this.checkHitBoxes(xPos + 90, yPos + 55, 121, 167)) {
+	      return true;
+	    }
+	    return false;
+	  },
+	
+	  checkBuildings: function (xPos, yPos) {
+	    if (this.checkHitBoxes(xPos + 15, yPos + 130, 377, 274) || this.checkHitBoxes(xPos + 30, yPos + 70, 251, 61) || this.checkHitBoxes(xPos + 100, yPos + 20, 50, 50)) {
+	      return true;
+	    }
+	  },
+	
+	  checkPokeBalls: function (xPos, yPos) {
+	    if (this.checkHitBoxes(xPos, yPos, 22, 22)) {
+	      return true;
+	    }
+	    return false;
+	  },
+	
+	  checkHitBoxes: function (itemX, itemY, width, height) {
+	    if (!(this.pokemon.position.x > itemX + width || this.pokemon.position.x + this.pokemon.width < itemX || this.pokemon.position.y > itemY + height || this.pokemon.position.y + this.pokemon.height < itemY)) {
+	      return true;
 	    }
 	  },
 	
@@ -231,22 +254,22 @@
 	
 	  instantiateClouds: function () {
 	    this.cloud1 = new Image();
-	    this.cloud1.src = "../images/cloud1.png";
+	    this.cloud1.src = "./images/cloud1.png";
 	    this.cloud1.height = 200;
 	    this.cloud1.width = 300;
 	
 	    this.cloud2 = new Image();
-	    this.cloud2.src = "../images/cloud2.png";
+	    this.cloud2.src = "./images/cloud2.png";
 	    this.cloud2.height = 108;
 	    this.cloud2.width = 231;
 	
 	    this.cloud3 = new Image();
-	    this.cloud3.src = "../images/cloud3.png";
+	    this.cloud3.src = "./images/cloud3.png";
 	    this.cloud3.height = 108;
 	    this.cloud3.width = 214;
 	
 	    this.cloud4 = new Image();
-	    this.cloud4.src = "../images/cloud4.png";
+	    this.cloud4.src = "./images/cloud4.png";
 	    this.cloud4.height = 240;
 	    this.cloud4.width = 240;
 	  },
@@ -302,7 +325,7 @@
 	
 	  makeUpSprite: function () {
 	    const pkmnSpriteUp = new Image();
-	    pkmnSpriteUp.src = "../images/bflyup.png";
+	    pkmnSpriteUp.src = "./images/bflyup.png";
 	
 	    this.spriteUp = {
 	      image: pkmnSpriteUp
@@ -311,7 +334,7 @@
 	
 	  makeDownSprite: function () {
 	    const pkmnSpriteDown = new Image();
-	    pkmnSpriteDown.src = "../images/bflydown.png";
+	    pkmnSpriteDown.src = "./images/bflydown.png";
 	
 	    this.spriteDown = {
 	      image: pkmnSpriteDown
@@ -444,7 +467,7 @@
 	  this.constantVelocity = 5;
 	
 	  const building = new Image();
-	  building.src = "../images/building.png";
+	  building.src = "./images/building.png";
 	
 	  this.building = {
 	    image: building
@@ -461,12 +484,11 @@
 	  goBuildingGo: function () {
 	
 	    this.context.clearRect(this.position.x, this.position.y, 402, 548);
-	    this.position.x -= this.constantVelocity;
+	    this.position.x = this.constantVelocity;
 	
-	    if (!this.game.over() && this.position.x >= -1200) {
+	    if (!this.game.over() && this.position.x >= 1200) {
 	      this.drawImg(this.position.x, this.position.y);
 	    } else if (this.game.over()) {
-	      this.game.overScreen();
 	      window.clearInterval(this.buildingInterval);
 	    }
 	  },
@@ -495,7 +517,7 @@
 	  this.constantVelocity = 5;
 	
 	  const balloon = new Image();
-	  balloon.src = "../images/balloon.png";
+	  balloon.src = "./images/balloon.png";
 	
 	  this.balloon = {
 	    image: balloon
@@ -511,9 +533,9 @@
 	
 	  goBalloonGo: function () {
 	    this.context.clearRect(this.position.x, this.position.y, 340, 234);
-	    this.position.x -= this.constantVelocity;
+	    this.position.x = this.constantVelocity;
 	
-	    if (!this.game.over() && this.position.x >= -400) {
+	    if (!this.game.over() && this.position.x >= 400) {
 	      this.drawImg(this.position.x, this.position.y);
 	    } else if (this.game.over()) {
 	      this.game.overScreen();
@@ -555,7 +577,7 @@
 	  gameIsOverBuddy: function () {
 	    this.context.clearRect(0, 0, 800, 1000);
 	    const over = new Image();
-	    over.src = "../images/gameOver.png";
+	    over.src = "./images/gameOver.png";
 	    over.onload = function () {
 	      this.context.drawImage(over, 260, 300);
 	      this.revealScore();
@@ -566,11 +588,11 @@
 	    let whichImage = new Image();
 	
 	    if (this.currentScore <= 6) {
-	      whichImage.src = "../images/bronze.png";
+	      whichImage.src = "./images/bronze.png";
 	    } else if (this.currentScore <= 20) {
-	      whichImage.src = "../images/silver.png";
+	      whichImage.src = "./images/silver.png";
 	    } else {
-	      whichImage.src = "../images/gold.png";
+	      whichImage.src = "./images/gold.png";
 	    }
 	
 	    whichImage.onload = function () {
@@ -604,9 +626,9 @@
 	    const startButton = new Image();
 	    const instructions = new Image();
 	    const logo = new Image();
-	    startButton.src = "../images/startButton.png";
-	    instructions.src = "../images/instructions.png";
-	    logo.src = "../images/logo.png";
+	    startButton.src = "./images/startButton.png";
+	    instructions.src = "./images/instructions.png";
+	    logo.src = "./images/logo.png";
 	    startButton.onload = function () {
 	      this.context.drawImage(logo, 130, 180);
 	      this.context.drawImage(startButton, 260, 400);
